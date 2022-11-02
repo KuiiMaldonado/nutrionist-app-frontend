b1f91374b89afc5db9472da24abd08ed73fee8f9
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-02 14:40:06 -0600

    Added component NotFound

---
+++ src/components/NotFound.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from "react";
+import {Container, Image} from "react-bootstrap";
+
+const NotFound = () => {
+    const[dogPath, setDogPath] = useState('');
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random').then(response => {
+            response.json().then(data => {
+                console.log(data);
+                setDogPath(data.message);
+            });
+        });
+    }, [])
+    return (
+        <>
+            <Container className={'text-center'}>
+                <h1 className={'mt-3 mb-3'}>Oooops! Page not found</h1>
+                <Image rounded className={'mb-3'} src={dogPath}></Image>
+            </Container>
+        </>
+    )
+}
+
+export default NotFound;
