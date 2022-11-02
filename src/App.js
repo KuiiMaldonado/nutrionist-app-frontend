c51692aa9e0b7b37d6289955994c3dc32242bc0c
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-02 14:04:46 -0600

    Removed BrowserRouter tag as we already have it in index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from "./pages/MainPage";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
@@ -13,8 +13,14 @@ import ProfilePage from "./pages/ProfilePage";
 import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
 import {setContext} from "@apollo/client/link/context";
 
+let uri;
+if (process.env.NODE_ENV === 'production')
+    uri = process.env.REACT_APP_GRAPH_SERVER;
+else
+    uri = 'http://localhost:3001/graphql';
+
 const httpLink = createHttpLink({
-    uri: process.env.REACT_APP_GRAPH_SERVER,
+    uri: uri,
 });
 
 const authLink = setContext((_, {headers}) => {
@@ -35,38 +41,33 @@ const client = new ApolloClient({
 function App() {
   return (
       <ApolloProvider client={client}>
-
           <div className={'metpro-app'}>
-              <Router>
-                  <>
-                      <NavigationBar/>
-                      <Routes>
-                          <Route
-                              path={'/'}
-                              element={<MainPage/>}
-                          />
-                          <Route
-                              path={'/meet-us'}
-                              element={<MeetUsPage/>}
-                          />
-                          <Route
-                              path={'/contact-us'}
-                              element={<ContactUsPage/>}
-                          />
-                          <Route
-                              path={'/login'}
-                              element={<LoginPage/>}
-                          />
-                          <Route
-                              path={'/profile'}
-                              element={<ProfilePage/>}
-                          />
-                      </Routes>
-                      <SocialMedia/>
-                      <Divider/>
-                      <Footer/>
-                  </>
-              </Router>
+              <NavigationBar/>
+              <Routes>
+                  <Route
+                      path={'/'}
+                      element={<MainPage/>}
+                  />
+                  <Route
+                      path={'/meet-us'}
+                      element={<MeetUsPage/>}
+                  />
+                  <Route
+                      path={'/contact-us'}
+                      element={<ContactUsPage/>}
+                  />
+                  <Route
+                      path={'/login'}
+                      element={<LoginPage/>}
+                  />
+                  <Route
+                      path={'/profile'}
+                      element={<ProfilePage/>}
+                  />
+              </Routes>
+              <SocialMedia/>
+              <Divider/>
+              <Footer/>
           </div>
       </ApolloProvider>
   );
