539d9a40eb8ca561975b6d6e845c675e2903afcc
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-08 14:53:40 -0600

    Created ManageUsers component

---
+++ src/components/ManageUsers.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Container, Row} from "react-bootstrap";
+
+const ManageUsers = () => {
+    return (
+      <Container>
+          <Row>
+              <h2>Manage users</h2>
+          </Row>
+          <Row>
+
+          </Row>
+      </Container>
+    );
+}
+
+export default ManageUsers;
