23d96ada45f0d60a4b6574571ce29785b21b34cc
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-08 18:41:58 -0600

    First desfing of manage user component

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -1,16 +1,65 @@
 import React from "react";
-import {Container, Row} from "react-bootstrap";
+import {Container, ListGroup, ListGroupItem, Row} from "react-bootstrap";
+import Auth from "../utils/auth";
+import {useQuery} from "@apollo/client";
+import {GET_ALL_USERS} from "../utils/queries";
+import LoadingSpinners from "./LoadingSpinners";
+import {faUserPen, faUserSlash, faUserPlus} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+import '../assets/css/ManageUsers.css';
 
 const ManageUsers = () => {
-    return (
-      <Container>
-          <Row>
-              <h2>Manage users</h2>
-          </Row>
-          <Row>
+    if (!Auth.loggedIn()) {
+        window.location.assign('/login');
+    }
 
-          </Row>
-      </Container>
+    if (!Auth.isAdmin()) {
+        window.location.assign('/profile');
+    }
+
+    const {loading, data} = useQuery(GET_ALL_USERS);
+
+    if (loading) {
+        return (
+            <LoadingSpinners/>
+        );
+    }
+    return (
+        <Container>
+            <Row>
+                <h2>Manage users</h2>
+            </Row>
+            <ListGroup as={'ul'}>
+                {data.users.map((user) => {
+                    return (
+                        <ListGroupItem key={user._id} as={'li'} className={'d-flex justify-content-between align-items-start'}>
+                            <div>
+                                <div>
+                                    <h6>{`${user.firstName} ${user.lastName}`}</h6>
+                                </div>
+                                {user.email}
+                            </div>
+                            <div>
+                                <button className={'user-button edit'}>
+                                    <FontAwesomeIcon icon={faUserPen} size={'xl'}/>
+                                </button>
+                                <button className={'user-button delete'}>
+                                    <FontAwesomeIcon icon={faUserSlash} size={'xl'}/>
+                                </button>
+                            </div>
+                        </ListGroupItem>
+                    );
+                })}
+                <ListGroupItem as={'li'}>
+                    <div className={'text-center'}>
+                        <button className={'user-button add'}>
+                            <FontAwesomeIcon icon={faUserPlus} size={'xl'}/>
+                        </button>
+                    </div>
+                </ListGroupItem>
+            </ListGroup>
+        </Container>
     );
 }
 
