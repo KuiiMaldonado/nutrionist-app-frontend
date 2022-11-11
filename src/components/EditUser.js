c7fc8f512e54c551c934fa11d07e47f8d4039b01
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-10 20:03:30 -0600

    Measures tab displaying correctly. TODO add and delete

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useLocation} from "react-router-dom";
 import Auth from "../utils/auth";
+import {Container, Row, Tabs, Tab} from "react-bootstrap";
+import {useQuery, useMutation} from "@apollo/client";
+import {GET_USER} from "../utils/queries";
+import Divider from "./Divider";
+import Measures from "./Measures";
+import LoadingSpinners from "./LoadingSpinners";
+
+import '../assets/css/ManageUsers.css';
 
 const EditUser = () => {
     if (!Auth.loggedIn()) {
@@ -13,8 +21,44 @@ const EditUser = () => {
 
     const {state} = useLocation();
     const {id} = state;
+    const {loading, data, refetch} = useQuery(GET_USER, {
+        variables: {userId: id}
+    });
+
+    useEffect(() => {
+       refetch().then(() => {
+
+       })
+    });
+
+    let userData;
+    if (!loading) {
+        userData = data.user;
+    }
+    else {
+        return (
+            <LoadingSpinners/>
+        );
+    }
+
     return (
-        <h1>Edit user {id}</h1>
+        <Container>
+            <Row>
+                <h2>Editing <span id={'profileName'}>{`${userData.firstName} ${userData.lastName}`}</span></h2>
+            </Row>
+            <Divider/>
+            <Tabs defaultActiveKey={'measures'} justify>
+                <Tab eventKey={'measures'} title={'Measures'}>
+                    <Measures measures={userData.userMeasures} edit={true}/>
+                </Tab>
+                <Tab eventKey={'diets'} title={'Diets'}>
+                    <h1>Diets</h1>
+                </Tab>
+                <Tab eventKey={'trainings'} title={'Trainings'}>
+                    <h1>Trainings</h1>
+                </Tab>
+            </Tabs>
+        </Container>
     );
 }
 
