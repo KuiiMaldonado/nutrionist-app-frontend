cafdf6e8ab1917bd415d70021bdd77a9069b4758
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-11-12 11:19:24 -0600

    Created AddMeasure component

---
+++ src/components/AddMeasure.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Container, Row} from "react-bootstrap";
+import Divider from "./Divider";
+
+const AddMeasure = () => {
+    return (
+        <Container>
+            <Row>
+                <h2>Add measure</h2>
+            </Row>
+            <Divider/>
+        </Container>
+    );
+}
+
+export default AddMeasure;
